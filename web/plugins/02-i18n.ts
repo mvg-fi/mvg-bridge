8d5d0e62f6a23b67de25bcdd2d30e0b0151b1da5
zedwong <kingflyme1@gmail.com>
2023-11-04 16:11:37 +0700

    add more i18n files

--- web/plugins/02-i18n.ts
+++ web/plugins/02-i18n.ts
@@ -1,26 +1,46 @@
 import { createI18n } from 'vue-i18n'
 import zh from '~/locales/zh.json'
 import en from '~/locales/en.json'
+import fr from '~/locales/fr.json'
+import es from '~/locales/es.json'
+import de from '~/locales/de.json'
+import ru from '~/locales/ru.json'
+import ar from '~/locales/ar.json'
 import zh_hk from '~/locales/zh-hk.json'
 
 export const langs = [
-  {name: "English", key: "en"},
-  {name: "简体中文", key: "zh"},
-  {name: "繁體中文", key: "zh_hk"},
+  { name: "English", key: "en" },
+  { name: "简体中文", key: "zh" },
+  { name: "繁體中文", key: "zh_hk" },
+  { name: "français", key: "fr" },
+  { name: "Deutsch", key: "de" },
+  { name: "Español", key: "es" },
+  { name: "ру́сский язы́к", key: "ru" },
+  { name: "اَلْعَرَبِيَّةُ", key: "ar" },  
 ]
 
-export const getNameByKey = (k: string) : string => {
-  switch(k) {
-  case "en":
-    return "English"
-  case "zh":
-    return "简体中文"
-  case "zh_hk":
-    return "繁體中文"
-  default:
-    return "English"
+export const getNameByKey = (k: string): string => {
+  switch (k) {
+    case "en":
+      return "English"
+    case "zh":
+      return "简体中文"
+    case "zh_hk":
+      return "繁體中文"
+    case "fr":
+      return "français"
+    case "de":
+      return "Deutsch"
+    case "es":
+      return "Español"
+    case "ru":
+      return "ру́сский язы́к"
+    case "ar":
+      return "اَلْعَرَبِيَّةُ"
+    default:
+      return "English"
   }
-} 
+}
 
 const i18n = createI18n({
   legacy: false,
@@ -29,7 +49,12 @@ const i18n = createI18n({
   messages: {
     en,
     zh,
-    zh_hk
+    zh_hk,
+    fr,
+    es,
+    de,
+    ru,
+    ar,
   }
 })
 
